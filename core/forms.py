f66e54a9d0fdea8259b420ba33ef9b51509e1a8d
Amankaium <kaium@list.ru>
2024-07-23 11:56:11 +0600

    added datepickers

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,23 @@ from django import forms
 from .models import Product
 
 
+class DatePicker(forms.DateInput):
+    input_type = 'date'
+
+
 class ProductForm(forms.ModelForm):
+    guarantee = forms.DateField(
+        widget=DatePicker,
+        label="Последний день гарантии",
+        required=False,
+    )
+    
+    expiration_date = forms.DateField(
+        widget=DatePicker,
+        label="Последний день срока годности",
+        required=False,
+    )
+    
     class Meta:
         model = Product
         fields = [
