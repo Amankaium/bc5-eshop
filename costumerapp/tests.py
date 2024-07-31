4467d20f8795a010815ce41907779132a2c0a362
Amankaium <kaium@list.ru>
2024-07-31 11:38:36 +0600

    add selenium test

--- costumerapp/tests.py
+++ costumerapp/tests.py
@@ -1,3 +1,29 @@
+from time import sleep
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-# Create your tests here.
+
+class CostumerTestCase(TestCase):
+    def test_signin(self):
+        path = r"C:\Users\user\projects\codify\bootcamp5\eshop\chromedriver\chromedriver.exe"
+        cService = webdriver.ChromeService(executable_path=path)
+        driver = webdriver.Chrome(service=cService)
+        driver.get("http://localhost:8000/signin/")
+        sleep(3)
+        username_element = driver.find_element(By.NAME, "username")
+        password_element = driver.find_element(By.ID, "id_password")
+        
+        username_element.clear()
+        username_element.send_keys("admin")
+        sleep(3)
+        
+        password_element.clear()
+        password_element.send_keys("admin")
+        sleep(3)
+        password_element.send_keys(Keys.RETURN)
+        assert "Вы успешно авторизовались!" in driver.page_source
+        sleep(5)
+        
+        driver.close()
